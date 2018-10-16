28ef6d27226e000e963dabb2db4c648a863017ad
罗帅 <1142909684@qq.com>
2018-10-16 15:48:46 +0800

    优化代码

--- src/store/getters.js
+++ src/store/getters.js
@@ -5,7 +5,6 @@ import { asyncRouterMap }  from '@/router'
 const getters = {
   sidebar: state => state.app.sidebar,
   userInfo: state => state.user.userInfo,
-<<<<<<< HEAD
   gysList: state => state.app.gysData,
   storeList: state => state.app.storeList,
   visitedViews: state => state.tagsView.visitedViews,
@@ -13,48 +12,44 @@ const getters = {
   companyId: state => state.user.companyId,
   permission_codes: state => state.user.permissionCodes,
 
-  // menu: state => {
-  //   return asyncRouterMap
-  // }
-=======
-  visitedViews: state => state.tagsView.visitedViews,
-  
->>>>>>> origin/develop
   menu: state => {
-    // const bakmenu = JSON.parse(state.user.userInfo.menus)
-    let bakmenu = JSON.parse(state.user.userInfo.menus)
-    let localPathArr=asyncRouterMap.map(v=>v.path)
-        if(Array.isArray(bakmenu)){
-          bakmenu=bakmenu.filter(v=>localPathArr.includes(v.path))
-        }
-    const menutemp = []
-    bakmenu.map(item => {
-      const subchildren = []
-      if (item.children && item.children.length) {
-        item.children.map(subitem => {
-          subchildren.push({
-            path: subitem.path,
-            component: subitem.component ? _import(subitem.component) : null,
-            name: subitem.code,
-            meta: JSON.parse(subitem.meta),
-            outLinkUrl: subitem.outLinkUrl ? reportCenterUrl(subitem.outLinkUrl) : '',
-            hidden: subitem.hidden === 'true'
-          })
-        })
-      }
-      menutemp.push({
-        path: item.path,
-        component: item.component === 'Layout' ? Layout : null,
-        redirect: item.redirect,
-        name: item.code,
-        outLinkUrl: item.outLinkUrl ? reportCenterUrl(item.outLinkUrl) : '',
-        hidden: item.hidden === 'true',
-        alwaysShow: item.alwaysShow,
-        meta: JSON.parse(item.meta),
-        children: subchildren
-      })
-    })
-    return menutemp
+    return asyncRouterMap
   }
+  // menu: state => {
+  //   // const bakmenu = JSON.parse(state.user.userInfo.menus)
+  //   let bakmenu = JSON.parse(state.user.userInfo.menus)
+  //   let localPathArr=asyncRouterMap.map(v=>v.path)
+  //       if(Array.isArray(bakmenu)){
+  //         bakmenu=bakmenu.filter(v=>localPathArr.includes(v.path))
+  //       }
+  //   const menutemp = []
+  //   bakmenu.map(item => {
+  //     const subchildren = []
+  //     if (item.children && item.children.length) {
+  //       item.children.map(subitem => {
+  //         subchildren.push({
+  //           path: subitem.path,
+  //           component: subitem.component ? _import(subitem.component) : null,
+  //           name: subitem.code,
+  //           meta: JSON.parse(subitem.meta),
+  //           outLinkUrl: subitem.outLinkUrl ? reportCenterUrl(subitem.outLinkUrl) : '',
+  //           hidden: subitem.hidden === 'true'
+  //         })
+  //       })
+  //     }
+  //     menutemp.push({
+  //       path: item.path,
+  //       component: item.component === 'Layout' ? Layout : null,
+  //       redirect: item.redirect,
+  //       name: item.code,
+  //       outLinkUrl: item.outLinkUrl ? reportCenterUrl(item.outLinkUrl) : '',
+  //       hidden: item.hidden === 'true',
+  //       alwaysShow: item.alwaysShow,
+  //       meta: JSON.parse(item.meta),
+  //       children: subchildren
+  //     })
+  //   })
+  //   return menutemp
+  // }
 }
 export default getters
