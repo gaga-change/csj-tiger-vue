a7cdcf35c4bcad9e140b2bb775b661e58f9f0c71
李松 <981174580@qq.com>
2018-10-25 17:27:40 +0800

    优化入库详情

---
+++ src/views/warehousing/businessorder/config.js
@@ -0,0 +1,50 @@
+export   const tableConfig=[
+  { label:'序号',type:'index',width:50},
+  { label:'SKU名称',prop:'skuName'},
+  { label:'规格型号',prop:'skuFormat'},
+  { label:'生产厂家',prop:'productFactory'},
+  { label:'商品分类',prop:'skuCategoryno'},
+  { label:'品牌名',prop:'skuBrandName'},
+  { label:'转换比',prop:'skuUnitConvert'},
+  { label:'出单价格',prop:'inPrice'},
+  { label:'已入/应入(单位)',fixed:false,dom:(row, column, cellValue, index)=>{
+      return `${row.realInQty}/${row.planInQty}(${row.skuUnitName})`
+  }},
+ 
+];
+
+export  const infoConfig=[
+  {title:'业务单号',prop:'busiBillNo',},
+  {title:'业务类型',prop:'busiBillType',type:'getBillType',useApi:true},
+  {title:'来源系统',prop:'fromSystemId'},
+  {title:'制单时间',prop:'gmtCreate',type:'time'},
+  {title:'制单人',prop:'createrName'},
+  {title:'状态',prop:'billState',type:'outbusibillstate',useApi:true},
+  {title:'产品线',prop:'productLine'},
+  {title:'业务模式',prop:'businessModel'},
+  {title:'供应商编号',prop:'providerCode'},
+  {title:'供应商名称',prop:'providerName'},
+  {title:'联系电话',prop:'providerLinkTel'},
+  {title:'其他信息',prop:'otherInfo'},
+]
+
+export const warehousingTableConfig=[
+  { label:'序号',type:'index',width:50},
+  { label:'入库单号',prop:'warehouseExeCode'},
+  { label:'货主',prop:'ownerName'},
+  { label:'计划单号',prop:'planCode'},
+  { label:'仓库',prop:'warehouseName'},
+  { label:'入库日期',prop:'inWarehouseTime',type:'time'},
+];
+
+export const planTableConfig=[
+  { label:'序号',type:'index',width:50},
+  { label:'计划单号',prop:'planCode'},
+  { label:'货主',prop:'ownerName'},
+  { label:'制单人',prop:'createrName'},
+  { label:'计划入库仓库',prop:'planWarehouseName'},
+  { label:'制定时间',prop:'gmtCreate',type:'time'},
+  { label:'计划入库日期',prop:'planTime',type:'time'},
+  { label:'下推状态',prop:'issuedState',type:'getIssuedState',useApi:true},
+  { label:'执行状态',prop:'execStatus',type:'outbusibillstate',useApi:true},
+]
--- src/views/warehousing/businessorder/detail.vue
+++ src/views/warehousing/businessorder/detail.vue
@@ -1,17 +1,10 @@
 <template>
   <div class="outgoing-quirydetail-container">
-      <div style="marginBottom:12px">
-     <item-title text="基本信息"/>
-      <el-card class="box-card" v-loading="loading"  element-loading-text="加载中..." shadow="never" body-style="padding:12px" >
-      <el-row>
-        <el-col  v-for="item in infoConfig"  :key="item.value"  :span="item.span" :style="item.style">
-             <span class="card-title">{{item.title}}</span> : <span class="card-text">{{formatter(item.type,config[item.value])}}</span>
-        </el-col>
-    </el-row>
-      </el-card>
-    </div>
 
-      <item-title text="相关明细"/>
+     <item-title text="基本信息"/>
+     <item-card :config="infoConfig" :loading="loading"   :cardData="config"  />
+
+     <item-title text="相关明细"/>
      <el-tabs v-model="tabActive" type="card" @tab-click="activeChange">
         <el-tab-pane label="相关业务单" name="businessorder">
           <web-pagination-table 
@@ -39,96 +32,39 @@
 </template>
 
 <script>
- import moment from 'moment';
  import { inBillDetail,inOrderSelect,inPlanSelect} from '@/api/warehousing';
  import webPaginationTable from '@/components/Table/webPaginationTable'
-
- const tableConfig=[
-    { label:'序号',type:'index',width:50},
-    { label:'SKU名称',prop:'skuName'},
-    { label:'规格型号',prop:'skuFormat'},
-    { label:'生产厂家',prop:'productFactory'},
-    { label:'商品分类',prop:'skuCategoryno'},
-    { label:'品牌名',prop:'skuBrandName'},
-    { label:'转换比',prop:'skuUnitConvert'},
-    { label:'出单价格',prop:'inPrice'},
-    { label:'已入/应入(单位)',fixed:false,dom:(row, column, cellValue, index)=>{
-        return `${row.realInQty}/${row.planInQty}(${row.skuUnitName})`
-    }},
-   
- ];
-
- const infoConfig=[
-   {title:'业务单号',value:'busiBillNo',style:'minWidth:310px;marginBottom:16px',span:6},
-   {title:'业务类型',type:'busiBillType',value:'busiBillType',style:'minWidth:310px;marginBottom:16px',span:6},
-   {title:'来源系统',value:'fromSystemId',style:'minWidth:310px;marginBottom:16px',span:6},
-   {title:'制单时间',type:'time',value:'gmtCreate',style:'minWidth:310px;marginBottom:16px',span:6},
-   {title:'制单人',value:'createrName',style:'minWidth:310px;marginBottom:16px',span:6},
-   {title:'状态',type:'busiBillState',value:'billState',style:'minWidth:310px;marginBottom:16px',span:6},
-   {title:'产品线',value:'productLine',style:'minWidth:310px;marginBottom:16px',span:6},
-   {title:'业务模式',value:'businessModel',style:'minWidth:310px;marginBottom:16px',span:6},
-   {title:'供应商编号',value:'providerCode',style:'minWidth:310px;marginBottom:16px',span:6},
-   {title:'供应商名称',value:'providerName',style:'minWidth:310px;marginBottom:16px',span:6},
-   {title:'联系电话',value:'providerLinkTel',style:'minWidth:310px;marginBottom:16px',span:6},
-   {title:'其他信息',value:'otherInfo',style:'minWidth:310px;marginBottom:16px',span:6},
- ]
-
+ import { tableConfig,infoConfig,warehousingTableConfig ,planTableConfig} from './config';
 
  export default {
     components: { webPaginationTable },
     data() {
       return {
+        loading:false,
+
         config:{},
         infoConfig,
         
         tableData:[],
-        tableConfig:tableConfig,
+        tableConfig,
         
-        busiBillTypeConfig:[],
-        busiBillStateConfig:[],
-        loading:false,
-
         warehousingLoding:false,
-        warehousingTableConfig:[],
+        warehousingTableConfig,
         warehousingTableData:[],
-        busiBillNo:'',
-        tabActive:'businessorder',
-
 
         planLoading:false,
-        planTableConfig:[],
+        planTableConfig,
         planTableData:[],
+
+        busiBillNo:'',
+        tabActive:'businessorder',
 
       }
     },
 
-    beforeMount(){
-      this.warehousingTableConfig=[
-        { label:'序号',type:'index',width:50},
-        { label:'入库单号',prop:'warehouseExeCode'},
-        { label:'货主',prop:'ownerName'},
-        { label:'计划单号',prop:'planCode'},
-        { label:'仓库',prop:'warehouseName'},
-        { label:'入库日期',prop:'inWarehouseTime',type:'time'},
-      ];
-
-      this.planTableConfig=[
-        { label:'序号',type:'index',width:50},
-        { label:'计划单号',prop:'planCode'},
-        { label:'货主',prop:'ownerName'},
-        { label:'制单人',prop:'createrName'},
-        { label:'计划入库仓库',prop:'planWarehouseName'},
-        { label:'制定时间',prop:'gmtCreate',type:'time'},
-        { label:'计划入库日期',prop:'planTime',type:'time'},
-        { label:'下推状态',prop:'issuedState',type:'getIssuedState',useApi:true},
-        { label:'执行状态',prop:'execStatus',type:'outbusibillstate',useApi:true},
-     ]
-    },
 
     mounted(){
-      let { id,busiBillTypeConfig,busiBillStateConfig}=this.$route.query.data&&JSON.parse(this.$route.query.data)||{};
-      this.busiBillTypeConfig=busiBillTypeConfig||[];
-      this.busiBillStateConfig=busiBillStateConfig||[];
+      let { id}=this.$route.query.data&&JSON.parse(this.$route.query.data)||{};
       this.loading=true;
       inBillDetail({id}).then(res=>{
         if(res.success){
@@ -144,20 +80,6 @@
     },
 
     methods:{
-      formatter(type,value,){
-        if(value!=undefined){
-          switch(type){
-            case 'time': return moment(value).format('YYYY-MM-DD HH:mm:ss');
-            case 'busiBillType': return this.busiBillTypeConfig.find(v=>v.key==value)&&this.busiBillTypeConfig.find(v=>v.key==value).value||'';
-            case 'busiBillState': return this.busiBillStateConfig.find(v=>v.key==value)&&this.busiBillStateConfig.find(v=>v.key==value).value||'';
-            case 'boolean': return Number(value)?'是':'否';
-            default : return value
-          }
-        } else{
-          return ''
-        }
-      },
-
       activeChange(tab){
         if(tab.name=='warehousing'){
             if(!this.warehousingTableData.length){
