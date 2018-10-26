b6953685df92989b0f7a7e38353c4f244dcabe4b
罗帅 <1142909684@qq.com>
2018-10-26 09:40:21 +0800

    在办待办测试 18-10-26

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="dashboard-container">
-     欢迎 {{userInfo.truename}} 进入交付中心管理平台
-     <!-- <el-row :gutter="24">
-      <el-col :span="15">
+     <!-- 欢迎 {{userInfo.truename}} 进入交付中心管理平台s -->
+     <el-row :gutter="24">
+      <el-col :span="24">
         <work-flow-node></work-flow-node>
       </el-col>
-      <el-col :span="9">
+      <!-- <el-col :span="9">
         <watch-message></watch-message>
-      </el-col>
-    </el-row>  -->
+      </el-col> -->
+    </el-row> 
   </div>
 </template>
 
