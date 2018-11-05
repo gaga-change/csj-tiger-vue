dc7544e05b32161ab8a59cb2d01ffd037eec7d84
罗帅 <1142909684@qq.com>
2018-11-05 19:33:03 +0800

    搜索 11-5

--- src/views/invoice/outputinvoice/components/search.vue
+++ src/views/invoice/outputinvoice/components/search.vue
@@ -3,33 +3,36 @@
       <el-form :model="searchForm" :rules="searchRules" ref="searchForm" label-width="70px" label-position="left">
       <el-row :gutter="10">
         <el-col :span="6">
-          <el-form-item label="客户名称">
-            <el-select v-model="searchForm.cusName" :clearable="true"   filterable placeholder="请选择客户名称" @change="customerChange"  >
+          <el-form-item label="客户名称"  prop="cusName">
+            <el-select v-model="searchForm.cusCode"
+              :filter-method="cusCodeFilter" 
+              clearable
+              filterable placeholder="请选择客户名称" @change="customerChange"  >
               <el-option 
                 value=""
                :disabled="true"
-               v-if="customerConfig.length>0" >
+               v-if="nowCustomerConfig.length>0" >
                 <span  class="codeNoStyle">企业编号</span>
                 <span  class="codeNoStyle" style="width:260px">企业名称</span>
               </el-option>
 
               <el-option
-                v-for="item in customerConfig"
-                :key="item.id"
-                :label="item.name"
-                :value="item.name">
-                <span class="codeNoStyle" >{{ item.code }}</span>
-                <span class="codeNoStyle" style="width:260px">{{ item.name }}</span>
+                v-for="item in nowCustomerConfig"
+                :key="item.entNumber"
+                :label="item.entName"
+                :value="item.entNumber">
+                <span class="codeNoStyle" >{{ item.entNumber }}</span>
+                <span class="codeNoStyle" style="width:260px">{{ item.entName }}</span>
               </el-option>
             </el-select>
           </el-form-item>
         </el-col>
 
-        <el-col :span="6">
+        <!-- <el-col :span="6">
           <el-form-item label="合同编号" >
             <el-input type="text" size="small" :disabled="true"  v-model="searchForm.contractNo" ></el-input>
           </el-form-item>
-        </el-col>
+        </el-col> -->
 
         <el-col :span="6" v-if="searchForm.searchItem=='apply'">
           <el-form-item label-width="120px" label="发票最迟开票日期:"  class="postInfo-container-item">
@@ -60,31 +63,34 @@
       </el-row>
       <el-row :gutter="10">  
         <el-col :span="6">
-          <el-form-item label="订单编号" >
-             <el-select v-model="searchForm.outBusiBillNo" :clearable="true"   filterable placeholder="请选择订单编号"   @change="saleorderChange" @focus="checkCustom" >
+          <el-form-item label="订单编号"  prop="orderNo" >
+             <el-select v-model="searchForm.orderNo" 
+              clearable
+              :filter-method="orderNoFilter" 
+              filterable placeholder="请选择订单编号" 
+              @focus="isHasCusName"
+              @change="saleorderChange" >
               <el-option 
                 value=""
                 :disabled="true" 
-                v-if="codeConfig.length>0" >
+                v-if="nowOrderNoConfig.length>0" >
                 <span  class="codeNoStyle">销售订单号</span>
                 <span  class="codeNoStyle">销售合同号</span>
-                <span  class="codeNoStyle" >物料名称</span>
               </el-option>
               <el-option
-                v-for="item in codeConfig"
+                v-for="item in nowOrderNoConfig"
                 :key="item.saleorder"
-                :label="item.saleorder"
-                :value="item.saleorder">
-                <span class="codeNoStyle">{{ item.saleorder }}</span>
-                <span class="codeNoStyle">{{ item.contractno }}</span>
-                <span class="codeNoStyle">{{ item.materialname}}</span>
+                :label="item.busiBillNo"
+                :value="item.busiBillNo">
+                <span class="codeNoStyle">{{ item.busiBillNo }}</span>
+                <span class="codeNoStyle">{{ item.contractNo }}</span>
               </el-option>
             </el-select>
           </el-form-item>
         </el-col>
          <el-col :span="6" v-if="searchForm.searchItem=='apply'">
           <el-form-item label="开票申请单号" label-width="90px">
-            <el-input type="text" size="small" :disabled="true"  v-model="searchForm.applyCode" ></el-input>
+            <el-input type="text" size="small"  v-model="searchForm.applyCode" ></el-input>
           </el-form-item>
         </el-col>
          <el-col :span="6" v-if="searchForm.searchItem=='register'">
@@ -176,6 +182,7 @@
 import { NatureInvoice, NatureInvoiceEnum, InvoiceStatus, TicketStatus, InvoiceType  as invoicetype } from '@/utils/enum'
 import { getGys } from '@/api/planorder';
 import { createOutputInvoice } from '@/api/invoice';
+import { infoCustomerInfo ,ordernoandcontractno,getSigningInformation,getSigningDetail,infoTaxno,saveFinaSaleInvoice,billingTypeDetails } from '@/api/newoutputinvoice';  
 import _  from 'lodash';
 
 export default {
@@ -190,8 +197,15 @@ export default {
       NatureInvoice,
       NatureInvoiceEnum,
       TicketStatus,
-      customerConfig:[],
       codeConfig:[],
+      customerConfig:[], //客户名称下拉配置
+      customerFilterMark:'',//客户名称过滤标识
+
+      orderNoConfig:[],//订单编号下拉配置
+      orderNoFilterMark:'',//订单编号过滤标识
+
+      signNoConfig:[],//签收单号下拉配置
+
     }
   },
   props:{
@@ -241,36 +255,110 @@ export default {
   created(){
     
     this.searchForm = _.cloneDeep(this.searchForms)
-     getGys().then(res=>{
-       this.customerConfig=res.data
+     infoCustomerInfo().then(res=>{
+        if(res.success){
+          this.customerConfig=res.data
+        }
      }).catch(err=>{
 
      })
   },
 
-  computed: {
-  
-  },
+   computed: {
+    nowCustomerConfig:{
+       get: function () {
+        let value=this.customerFilterMark;
+        if(value==''||!this.customerConfig.length){
+          return this.customerConfig
+        } else{
+          return this.customerConfig.filter(v=>v.entNumber.includes(value)||v.entName.includes(value))
+        }
+       },
+       set:function(){
+ 
+       }
+    },
+
+    nowOrderNoConfig:{
+      get: function () {
+        let value=this.orderNoFilterMark;
+        if(value==''||!this.orderNoConfig.length){
+          return this.orderNoConfig
+        } else{
+          return this.orderNoConfig.filter(v=>v.busiBillNo.includes(value)||v.contractNo.includes(value))
+        }
+       },
+       set:function(){
+ 
+       }
+    },
+   },
   
   methods:{
-    
-    customerChange(value){
-      this.createOutputInvoiceApi({customer:value})
+    cusCodeFilter(value){
+       this.customerFilterMark=value;
     },
-    checkCustom(e){    
-      if(!this.searchForm.cusName){
-        this.$message({type:'error',message:'请先选择客户',duration:2000})
+    orderNoFilter(value){
+      this.orderNoFilterMark=value;
+    },
+    isHasCusName(){
+      if(this.searchForm.cusName==''){
+        this.$message.error('请先选择客户名称');
       }
     },
-    saleorderChange(value){//订单编号
+
+    isShouDetails(){
+      if(this.searchForm.cusName==''){
+        this.$message.error('请先选择客户名称');
+      }else if(this.searchForm.orderNo==''){
+        this.$message.error('请先选择订单编号');
+      } else{
+         this.shouDetails=true;
+      }
+    },
+     customerChange(value){
       let searchForm=_.cloneDeep(this.searchForm);
-      searchForm.ContractNumber=this.codeConfig.find(v=>v.saleorder==value)&&this.codeConfig.find(v=>v.saleorder==value).contractno;//合同编号
+      searchForm.cusName=this.customerConfig.find(v=>v.entNumber==value)&&this.customerConfig.find(v=>v.entNumber==value).entName;
+      searchForm.orderNo='';
+      this.id='';
+      this.tableData=[];
       this.searchForm=searchForm;
+      this.ordernoandcontractnoApi({entNumber:value})
+    },
+
+    saleorderChange(value){
+      let searchForm=_.cloneDeep(this.searchForm);
+      searchForm.contractNo=this.orderNoConfig.find(v=>v.busiBillNo==value)&&this.orderNoConfig.find(v=>v.busiBillNo==value).contractNo;
+      this.searchForm=searchForm;
+      this.id='';
+      this.tableData=[];
+
+      getSigningInformation({
+        signatureNumber:value
+      }).then(res=>{
+        if(res.success){
+          this.signNoConfig=res.data;
+        }
+      }).catch(err=>{
+        
+      });
+
+      billingTypeDetails({
+        outBusiBillNo:value
+      }).then(res=>{
+        if(res.success){
+          this.outBusiBillNoConfig=res.data;
+        }
+      }).catch(err=>{
+
+      })
     },
     
-    createOutputInvoiceApi(data){
-       createOutputInvoice(data).then(res=>{
-          this.codeConfig=res.data;
+    ordernoandcontractnoApi(data){
+      ordernoandcontractno(data).then(res=>{
+        if(res.success){
+          this.orderNoConfig=res.data;
+         }
        }).catch(err=>{
 
        })
