ab9f023df41a07c39dd682981c477a5ad34dbed7
罗帅 <1142909684@qq.com>
2018-12-17 10:48:40 +0800

    付款登记 12-17

--- src/views/pay/register/index.vue
+++ src/views/pay/register/index.vue
@@ -45,7 +45,7 @@
         tableData: [],
         tableConfig:[],
         infoConfig:[ 
-          { title:'已收货款合计', prop:'totalPaymentAmt',type:'money' },
+          { title:'已付货款合计', prop:'totalPaymentAmt',type:'money' },
           { title:'贴息合计', prop:'totalInterestAmt',type:'money' },
         ],
         cardData:{},
