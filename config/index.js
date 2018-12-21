b0632f4444a62ec215599443202fc8b22e6963c4
罗帅 <1142909684@qq.com>
2018-12-21 14:45:57 +0800

    test 12-21

--- config/index.js
+++ config/index.js
@@ -1,22 +1,13 @@
 'use strict'
 
 const path = require('path')
-<<<<<<< HEAD
-// const ip='http://172.16.81.26:7878';//苗哥
-// const ip='http://172.16.81.162:7879';//修号
-=======
 const ip='http://172.16.81.26:7878';//苗哥
 // const ip='http://192.168.2.172:7878';//修号
->>>>>>> origin/dev-lisong-provider
 // const ip='http://192.168.2.163:7878';//红兵
 // const ip='http://192.168.2.139:7878';//陈姐
 // const ip='http://172.16.80.138:7878';//亚雷
 // const ip='http://172.16.80.83:7878';//亚超
-<<<<<<< HEAD
-const ip='http://192.168.1.195:7979';//测试服务
-=======
 // const ip='http://192.168.1.195:7979';//测试服务
->>>>>>> origin/dev-lisong-provider
 // const ip='http://172.16.81.219:7878';//龙
 
 const proxyTableArr=['/csj_login','/csj_logout','/webApi','/api'];
