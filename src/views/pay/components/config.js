6bbf836d779d17a186a096a6ceff242a87ae9419
罗帅 <1142909684@qq.com>
2018-12-28 16:27:57 +0800

    付款备注 12-28

--- src/views/pay/components/config.js
+++ src/views/pay/components/config.js
@@ -55,6 +55,7 @@ export const paymentInfoConfig = [//付款单货款详情
   { title:'收款方收款银行 ', prop:'receiveBank', paytype:'goods+service' },
   { title:' 附件 ', prop:'filePathList', useIf:'files', paytype:'goods+service' },
   { title:' 合同附件 ', prop:'contractFilePathList', useIf:'files', paytype:'goods+service' },
+  { title:'备注', prop:'remarkInfo', paytype:'goods+service'},
   // { title:'测试1 ', prop:'linkParam',linkTo:'/',useIf:'linkParam',name:'fuck' },
 ]
 
