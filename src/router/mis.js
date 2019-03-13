7b7492d9be536713b92e82fd23ae53fd3a0e2a46
李松 <981174580@qq.com>
2019-03-13 16:45:09 +0800

    提交暂存“

--- src/router/mis.js
+++ src/router/mis.js
@@ -25,5 +25,35 @@ export default [
         meta: {title: '新建配送单'}
       }
     ]
+  },
+
+  {
+    path: '/carrier',
+    component: Layout,
+    redirect: '/carrier/list',
+    name: 'carrier',
+    mark:'仓储运营',
+    meta: { title: '承运商', icon: 'sale' },
+    children: [
+      {
+        path: 'list',
+        component: _import('mis/carrier/list'),
+        name: 'carrierList',
+        meta: { title: '承运商管理'}
+      },
+      {
+        path: 'add',
+        component: _import('mis/carrier/add'),
+        name: 'carrierAdd',
+        meta: { title:'创建承运商' }
+      },
+      {
+        path: 'detail',
+        component: _import('mis/carrier/detail'),
+        name: 'carrierDetail',
+        hidden: true,
+        meta: { title:'承运商详情' }
+      },
+    ]
   }
 ]
---
+++ src/views/mis/carrier/components/config.js
@@ -0,0 +1,21 @@
+export  const carrierListConfig=[ 
+  { label:'序号',type:'index',width:50},
+  { label:'承运商编码',prop:''},
+  { label:'承运商名称',prop:''},
+  { label:'承运商状态',prop:''},
+  { label:'联系人',prop:''},
+  { label:'联系电话',prop:''},
+  { label:'操作',fixed:'right',useLink:true,width:180},
+]
+
+export  const carrierDetailConfig=[ 
+  { title:'承运商名称',prop:''},
+  { title:'承运商状态',prop:''},
+  { title:'结算区分',prop:''},
+  { title:'联系电话',prop:''},
+  { title:'联系人',prop:''},
+  { title:'合作起始日期',prop:'',type:'tiem'},
+  { title:'合同起始日期',prop:'',type:'tiem'},
+  { title:'合同结束日期',prop:'',type:'tiem'},
+  { title:'支付约定',prop:''}
+]
