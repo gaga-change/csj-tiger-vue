e58ecd5a8abfd3d80b779a9316c1d21369829632
李松 <981174580@qq.com>
2019-03-21 09:14:14 +0800

    提交暂存

---
+++ src/views/mis/supplierManagement/components/config.js
@@ -0,0 +1,9 @@
+export  const carrierListConfig=[ 
+  { label:'序号',type:'index',width:50},
+  { label:'供应商编码',prop:''},
+  { label:'承运商名称',prop:''},
+  { label:'承运商状态',prop:''},
+  { label:'物联网管理区分',prop:''},
+  { label:'单据状态',prop:''},
+  { label:'操作',fixed:'right',useLink:true,width:180},
+]
