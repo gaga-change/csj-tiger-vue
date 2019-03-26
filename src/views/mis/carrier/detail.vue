2f947fd04d02af68265f3de1a0776586e4a012f5
李松 <981174580@qq.com>
2019-03-26 16:38:11 +0800

    提交暂存

--- src/views/mis/carrier/detail.vue
+++ src/views/mis/carrier/detail.vue
@@ -5,6 +5,8 @@
           <router-link  :to="`/carrier/add?consoildatorCode=${this.$route.query.consoildatorCode}&id=${this.$route.query.id}&type=modify`"  class="tableLink">
             <el-button  type="success" size="small">修改</el-button>
          </router-link>
+
+         <el-button  size="small" @click="onDelete">删除</el-button>
       </template>
     </sticky>
      <div>
@@ -17,8 +19,9 @@
 <script>
 
 import { carrierDetailConfig  } from './components/config';
-import { consoilInfoDetail } from '@/api/carrier'
+import { consoilInfoDetail,consoilInfoDel} from '@/api/carrier'
 import Sticky from '@/components/Sticky'
+import { mapGetters } from 'vuex'
 export default {
   components: {Sticky},
    data() {
@@ -28,6 +31,14 @@ export default {
       baseinfoData:{},
     }
   },
+
+  computed: {
+      ...mapGetters({
+        'mapConfig':'mapConfig',
+         visitedViews: 'visitedViews'
+      })
+  },
+
 
   mounted(){
     consoilInfoDetail({
@@ -42,7 +53,34 @@ export default {
   },
 
   methods:{
-  
+    onDelete(){
+       const view = this.visitedViews.filter(v => v.path === this.$route.path)
+       consoilInfoDel({
+          carrierNo:this.$route.query.consoildatorCode
+        }).then(res=>{
+          if(res.success){
+             this.$message({
+                type:'success', 
+                message:'操作成功,即将跳转到列表页！' ,
+                duration:1500,
+                onClose:()=>{
+                  this.$store.dispatch('delVisitedViews', view[0]).then(() => {
+                    this.$router.push({
+                      path:`/carrier/list`,
+                    })
+                  }).catch(err=>{
+                    console.log(err)
+                  })  
+                }
+            })
+          } else{
+             this.$message.error('操作失败')
+          }
+        }).catch(err=>{
+          console.log(err)
+          this.$message.error('操作失败')
+      })
+    }
   }
 }
 </script>
