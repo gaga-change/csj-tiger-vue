2831480b0ef01c0a842ee03812bad603ee0b5970
严俊东 <1172482914@qq.com>
2019-09-05 19:44:28 +0800

    挂起类型 - 【商品不足】显示异常修复2

--- src/views/outgoing/plan/index.vue
+++ src/views/outgoing/plan/index.vue
@@ -408,7 +408,7 @@
             class="c-red"
           >
             {{
-              scope.row[column.prop] | apiEnum(column.type)
+              scope.row[column.prop] | apiEnum(mapConfig, column.type)
             }}
           </span>
           <span v-else-if="column.type === 'index'">{{ scope.$index + 1 }}</span>
