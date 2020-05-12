8ae61c456488b1302bc5ca73c6aedc05dfab4025
严俊东 <1172482914@qq.com>
2020-05-12 14:24:23 +0800

    modify

--- config/index.js
+++ config/index.js
@@ -29,8 +29,8 @@ module.exports = {
     proxyTable: proxyTable,
 
     // Various Dev Server settings
-    // host: '0.0.0.0', // can be overwritten by process.env.HOST
-    host: '192.168.2.117', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
+    // host: '192.168.2.117', // can be overwritten by process.env.HOST
     port: 7878, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
