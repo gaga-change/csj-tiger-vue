7b874c4dd02d95c46b406a1e0516a2ff63cff94b
严俊东 <1172482914@qq.com>
2020-05-19 11:49:18 +0800

    调拨单详情添加新属性

--- src/views/requisition/requisitionplan/config.js
+++ src/views/requisition/requisitionplan/config.js
@@ -1,28 +1,3 @@
-export const tableConfig = [
-  { label: '序号', type: 'index', width: 50 },
-  { label: '商品编码', prop: 'skuCode' },
-  { label: '商品名称', prop: 'skuName', width: 120 },
-  { label: '调拨数量', prop: 'transferQty' },
-  { label: '已出库数量', prop: 'realOutQty' },
-  { label: '已出库金额', prop: 'realOutAmt' },
-  { label: '已入库数量', prop: 'realInQty' },
-  { label: '已入库金额', prop: 'realInAmt' },
-  { label: '备注', prop: 'remarkInfo' }
-];
-
-export const infoConfig = [
-  { title: '调拨单号', prop: 'transferNo' },
-  { title: '货主', prop: 'ownerName' },
-  { title: '调出仓库', prop: 'outWarehouseName' },
-  { title: '调出日期', prop: 'outDate', type: 'time' },
-  { title: '调入仓库', prop: 'inWarehouseName' },
-  { title: '调入日期', prop: 'inDate', type: 'time' },
-  { title: '接收人', prop: 'warehouseLinkName' },
-  { title: '所在地区', prop: 'area' },
-  { title: '详细地址', prop: 'warehouseAddress' },
-  { title: '联系方式', prop: 'linkTel' },
-  { title: '备注', prop: 'remarkInfo', span: 12 },
-]
 
 export const indexTableConfig = [
   { label: '调拨单号', fixed: true, prop: 'transferNo', width: 160 },
@@ -49,19 +24,4 @@ export const addtable_config = [
 ]
 
 
-export const detail_planTableConfig = [
-  { label: '序号', type: 'index', fixed: true, width: 50 },
-  { label: '业务类型', prop: 'busiBillType', type: 'billTypeFilter', useLocalEnum: true },
-  { label: '计划单号', prop: 'planCode' },
-  { label: '数量', prop: 'planQty' },
-  { label: '金额', prop: 'planAmt' }
-]
 
-export const detail_outGoingTableConfig = [
-  { label: '序号', type: 'index', fixed: true, width: 50 },
-  { label: '业务类型', prop: 'busiBillType', type: 'billTypeFilter', useLocalEnum: true },
-  { label: '单号', prop: 'warehouseExeCode' },
-  { label: '计划单号', prop: 'planCode' },
-  { label: '数量', prop: 'planQty' },
-  { label: '金额', prop: 'planAmt' }
-]
--- src/views/requisition/requisitionplan/detail.vue
+++ src/views/requisition/requisitionplan/detail.vue
@@ -1,111 +1,174 @@
 <template>
   <div class="outgoing-quirydetail-container">
-     <item-title text="基本信息"/>
-     <item-card :config="infoConfig" :loading="loading"   :cardData="infoData"  />
+    <item-title text="基本信息" />
+    <item-card
+      :config="infoConfig"
+      :loading="loading"
+      :cardData="infoData"
+    />
 
-     <item-title text="相关明细"/>
-     <el-tabs v-model="activeName" type="card">
-      <el-tab-pane label="调拨单" name="business">
+    <item-title text="相关明细" />
+    <el-tabs
+      v-model="activeName"
+      type="card"
+    >
+      <el-tab-pane
+        label="调拨单"
+        name="business"
+      >
         <edit-Table
-        :loading="loading"
-        :config="detailTableConfig"
-        :allTableData="tableData"/>
+          :loading="loading"
+          :config="detailTableConfig"
+          :allTableData="tableData"
+        />
       </el-tab-pane>
-      <el-tab-pane label="计划单" name="plan">
+      <el-tab-pane
+        label="计划单"
+        name="plan"
+      >
         <edit-Table
-        :loading="loading"
-        :config="detail_planTableConfig"
-        :allTableData="detail_planTableData"/>
+          :loading="loading"
+          :config="detail_planTableConfig"
+          :allTableData="detail_planTableData"
+        />
       </el-tab-pane>
-      <el-tab-pane label="出/入库单" name="outgonging">
+      <el-tab-pane
+        label="出/入库单"
+        name="outgonging"
+      >
         <edit-Table
-        :loading="loading"
-        :config="detail_outGoingTableConfig"
-        :allTableData="detail_outGoingTableData"/>
+          :loading="loading"
+          :config="detail_outGoingTableConfig"
+          :allTableData="detail_outGoingTableData"
+        />
       </el-tab-pane>
-     </el-tabs>
+    </el-tabs>
   </div>
 </template>
 
 <script>
 
- import { tableConfig,infoConfig,detail_planTableConfig,detail_outGoingTableConfig} from './config';
- import editTable from '@/components/Table/editTable';
- import Sticky from '@/components/Sticky'
- import moment from 'moment';
- import { mapGetters } from 'vuex'
- import { requisitiondetail, queryPlandetail, queryOrderdetail } from '@/api/requisition'
- // import { operation } from './conpoments/lib';
- import _  from 'lodash';
+const detail_planTableConfig = [
+  { label: '序号', type: 'index', fixed: true, width: 50 },
+  { label: '业务类型', prop: 'busiBillType', type: 'billTypeFilter', useLocalEnum: true },
+  { label: '计划单号', prop: 'planCode' },
+  { label: '数量', prop: 'planQty' },
+  { label: '金额', prop: 'planAmt' }
+]
 
- export default {
-    components: { editTable,Sticky},
-    data() {
-      return {
-        loading:false,
-        infoData:{},
-        infoConfig,
 
-        tableData:[],
-        detailTableConfig:tableConfig,
-        activeName:'business',
+const detail_outGoingTableConfig = [
+  { label: '序号', type: 'index', fixed: true, width: 50 },
+  { label: '业务类型', prop: 'busiBillType', type: 'billTypeFilter', useLocalEnum: true },
+  { label: '单号', prop: 'warehouseExeCode' },
+  { label: '计划单号', prop: 'planCode' },
+  { label: '数量', prop: 'planQty' },
+  { label: '金额', prop: 'planAmt' }
+]
 
-        detail_planTableConfig,
-        detail_planTableData:[],
+const infoConfig = [
+  { title: '调拨单号', prop: 'transferNo' },
+  { title: '货主', prop: 'ownerName' },
+  { title: '调出仓库', prop: 'outWarehouseName' },
+  { title: '调出日期', prop: 'outDate', type: 'time' },
+  { title: '调入仓库', prop: 'inWarehouseName' },
+  { title: '调入日期', prop: 'inDate', type: 'time' },
+  { title: '接收人', prop: 'warehouseLinkName' },
+  { title: '所在地区', prop: 'area' },
+  { title: '详细地址', prop: 'warehouseAddress' },
+  { title: '联系方式', prop: 'linkTel' },
+  { title: '备注', prop: 'remarkInfo', span: 12 },
+  { title: '单据组织', prop: 'billOrganize' },
+  { title: '单据类型', prop: 'orderType', type: 'enum', enum: 'busiBillTypeEnum' },
+  { title: '调出组织', prop: 'inOrganize' },
+  { title: '调入组织', prop: 'outOrganize' },
+]
 
-        detail_outGoingTableConfig,
-        detail_outGoingTableData:[],
-      }
-    },
+const tableConfig = [
+  { label: '序号', type: 'index', width: 50 },
+  { label: '商品编码', prop: 'skuCode' },
+  { label: '商品名称', prop: 'skuName', width: 120 },
+  { label: '调拨数量', prop: 'transferQty' },
+  { label: '已出库数量', prop: 'realOutQty' },
+  { label: '已出库金额', prop: 'realOutAmt' },
+  { label: '已入库数量', prop: 'realInQty' },
+  { label: '已入库金额', prop: 'realInAmt' },
+  { label: '备注', prop: 'remarkInfo' }
+];
 
-    created(){
-      this.getCurrentTableData()
-    },
+import editTable from '@/components/Table/editTable';
+import Sticky from '@/components/Sticky'
+import moment from 'moment';
+import { mapGetters } from 'vuex'
+import { requisitiondetail, queryPlandetail, queryOrderdetail } from '@/api/requisition'
+// import { operation } from './conpoments/lib';
+import _ from 'lodash';
 
-    computed: {
-      ...mapGetters({
-         visitedViews: 'visitedViews'
-      })
-    },
+export default {
+  components: { editTable, Sticky },
+  data() {
+    return {
+      loading: false,
+      infoData: {},
+      infoConfig,
+      tableData: [],
+      detailTableConfig: tableConfig,
+      activeName: 'business',
+      detail_planTableConfig,
+      detail_planTableData: [],
+      detail_outGoingTableConfig,
+      detail_outGoingTableData: [],
+    }
+  },
 
-    methods:{
-       moment,
-       // operation,
-       getCurrentTableData(){
-          requisitiondetail({id:this.$route.query.id}).then(res=>{
-            if(res.success){
-              let data=res.data
-              this.infoData=res.data
-              this.infoData.area=data.warehouseProvince?(data.warehouseProvince+data.warehouseCity+data.warehouseArea):null
-              this.tableData=(res.data.transferBillDetailDOList && res.data.transferBillDetailDOList.length>0)?res.data.transferBillDetailDOList:[]
-              // 查询相关出入库计划单
-              queryPlandetail({transferNo:data.transferNo,}).then(res=>{
-                if(res.success){
-                  this.detail_planTableData=(res.data && res.data.length>0)?res.data:[]
-                }
-              }).catch(err=>{
-                console.error(err)
-              })
-              //查询相关出入库单
-              queryOrderdetail({transferNo:data.transferNo,}).then(res=>{
-                if(res.success){
-                  this.detail_outGoingTableData=(res.data && res.data.length>0)?res.data:[]
-                }
-              }).catch(err=>{
-                console.error(err)
-              })
+  created() {
+    this.getCurrentTableData()
+  },
+
+  computed: {
+    ...mapGetters({
+      visitedViews: 'visitedViews'
+    })
+  },
+
+  methods: {
+    moment,
+    // operation,
+    getCurrentTableData() {
+      requisitiondetail({ id: this.$route.query.id }).then(res => {
+        if (res.success) {
+          let data = res.data
+          this.infoData = res.data
+          this.infoData.area = data.warehouseProvince ? (data.warehouseProvince + data.warehouseCity + data.warehouseArea) : null
+          this.tableData = (res.data.transferBillDetailDOList && res.data.transferBillDetailDOList.length > 0) ? res.data.transferBillDetailDOList : []
+          // 查询相关出入库计划单
+          queryPlandetail({ transferNo: data.transferNo, }).then(res => {
+            if (res.success) {
+              this.detail_planTableData = (res.data && res.data.length > 0) ? res.data : []
             }
-          }).catch(err=>{
+          }).catch(err => {
             console.error(err)
           })
-       }
+          //查询相关出入库单
+          queryOrderdetail({ transferNo: data.transferNo, }).then(res => {
+            if (res.success) {
+              this.detail_outGoingTableData = (res.data && res.data.length > 0) ? res.data : []
+            }
+          }).catch(err => {
+            console.error(err)
+          })
+        }
+      }).catch(err => {
+        console.error(err)
+      })
     }
- }
+  }
+}
 
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
-  .el-dialog__body{
-    padding-top: 12px;
-  }
+.el-dialog__body {
+  padding-top: 12px;
+}
 </style>
