e5715af6d5c6a8861f04db6084c666a084431ed2
严俊东 <1172482914@qq.com>
2020-06-08 17:40:00 +0800

    详情业务类型枚举调整

--- src/views/inventory/quiry/detail.vue
+++ src/views/inventory/quiry/detail.vue
@@ -1,48 +1,53 @@
 <template>
   <div class="outgoing-quirydetail-container">
- 
-  <item-title text="基本信息"/>
-  <item-card :config="infoConfig" :loading="loading"   :cardData="config"  />
 
-  <item-title text="相关明细"/>
-  <web-pagination-table 
-    :loading="loading"
-    :config="tableConfig" 
-    :allTableData="tableData"/> 
+    <item-title text="基本信息" />
+    <item-card
+      :config="infoConfig"
+      :loading="loading"
+      :cardData="config"
+    />
+
+    <item-title text="相关明细" />
+    <web-pagination-table
+      :loading="loading"
+      :config="tableConfig"
+      :allTableData="tableData"
+    />
 
   </div>
 </template>
 
 <script>
- import moment from 'moment';
- import {stockRecord} from '@/api/inventory';
- import webPaginationTable from '@/components/Table/webPaginationTable'
- import { tableConfig,infoConfig} from './config';
- export default {
-    components: { webPaginationTable },
-    data() {
-      return {
-        config:{},
-        infoConfig,
+import moment from 'moment';
+import { stockRecord } from '@/api/inventory';
+import webPaginationTable from '@/components/Table/webPaginationTable'
+import { tableConfig, infoConfig } from './config';
+export default {
+  components: { webPaginationTable },
+  data() {
+    return {
+      config: {},
+      infoConfig,
 
-        loading:false,
-        tableData:[],
-        tableConfig,
+      loading: false,
+      tableData: [],
+      tableConfig,
+    }
+  },
+  mounted() {
+    let { warehouseCode, skuCode } = this.$route.query || {};
+    this.loading = true;
+    stockRecord({ warehouseCode, skuCode }).then(res => {
+      this.loading = false;
+      if (res.success) {
+        this.config = res.data || {};
+        this.tableData = (res.data && res.data.items && Array.isArray(res.data.items)) ? res.data.items : []
       }
-    },
-    mounted(){
-      let { warehouseCode,skuCode}=this.$route.query||{};
-      this.loading=true;
-      stockRecord({warehouseCode,skuCode}).then(res=>{
-        this.loading=false;
-        if(res.success){
-          this.config=res.data||{};
-          this.tableData=(res.data&&res.data.items&&Array.isArray(res.data.items))?res.data.items:[]
-        } 
-      }).catch(err=>{
-          this.loading=false;
-      })
-    },
- }
+    }).catch(err => {
+      this.loading = false;
+    })
+  },
+}
 
 </script>
